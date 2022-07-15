65d4d75d760a70164893f53925414cb5ebb3c89a
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-14 22:35:43 -0400

    feat: adicionando função para validar os dados na hora de cadastrar um produto

---
+++ system-backend/src/helpers/validateProducts.ts
@@ -0,0 +1,12 @@
+import Joi from '@hapi/joi';
+
+export const createProductValidate = (data) => {
+	const productSchema = Joi.object({
+		type: Joi.string().required(),
+		name: Joi.string().required(),
+		quantity: Joi.number().required(),
+		price: Joi.number().required(),
+	});
+
+	return productSchema.validate(data);
+};

7236dbabbe2c3de586b369cf7ec49d34a43dfafe
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-15 23:43:27 -0400

    feat: configurando as rotas dos produtos

--- system-backend/src/routes/productRouter.ts
+++ system-backend/src/routes/productRouter.ts
@@ -4,3 +4,6 @@ import productController from '../controllers/productController';
 export const productRouter = express.Router();
 
 productRouter.post('/create', productController.create);
+productRouter.get('/read', productController.read);
+productRouter.put('/update/:id', productController.update);
+productRouter.delete('/delete/:id', productController.delete);

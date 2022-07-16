a53cc53f69eb86a8f8b5fedf0f118c065d84837f
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-15 23:40:23 -0400

    feat: adicionando rotas para criar, atualizar, deletar e pegar todos os produtos

--- system-backend/src/controllers/productController.ts
+++ system-backend/src/controllers/productController.ts
@@ -1,10 +1,63 @@
-import { createProductValidate } from '../helpers/validateProducts';
+import { Request, Response } from 'express';
+import { productValidate } from '../helpers/validateProducts';
+import Product from '../models/productModel';
 
 export default {
-	create: (req, res): string => {
-		const { error, value } = createProductValidate(req.body);
-		if (error) res.status(401).send('Hove um erro');
+	create: async (req: Request, res: Response): Promise<void> => {
+		const { error, value } = productValidate(req.body);
+		if (error) res.status(401).send(error);
 
-		return res.status(200).send('Tudo certo');
+		try {
+			const product = new Product(value);
+			await product.save();
+
+			res.status(200).send('Produto cadastrado!');
+		} catch (error) {
+			res.status(400);
+			console.log(error);
+		}
+	},
+
+	read: async (req: Request, res: Response): Promise<void> => {
+		const data = await Product.find({});
+
+		if (data.length === 0)
+			res.status(404).send('Nenhum produto encontrado!');
+
+		try {
+			res.status(200).json(data);
+		} catch (error) {
+			res.status(400);
+			console.log(error);
+		}
+	},
+	update: async (req: Request, res: Response): Promise<void> => {
+		const { error, value } = productValidate(req.body);
+		const productId = req.params.id;
+
+		if (error) res.status(401).send(error);
+		if (!productId) res.status(404).send('Nenhum id informado!');
+
+		await Product.findOneAndUpdate({ _id: productId }, value);
+
+		try {
+			res.status(200).json('Produto atualizado!');
+		} catch (error) {
+			res.status(400);
+			console.log(error);
+		}
+	},
+	delete: async (req: Request, res: Response): Promise<void> => {
+		const productId = req.params.id;
+		if (!productId) res.status(404).send('Nenhum id informado!');
+
+		await Product.deleteOne({ _id: productId });
+
+		try {
+			res.status(200).send('Produto deletado!');
+		} catch (error) {
+			res.status(400);
+			console.log(error);
+		}
 	},
 };

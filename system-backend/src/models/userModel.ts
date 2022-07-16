2682fc908aeaeead6570861732398ff8c2043f88
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-16 10:35:46 -0400

    feat: criando o modelo dos dados do usuário com o mongoose

---
+++ system-backend/src/models/userModel.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+import { IUser } from '../interfaces/IUser';
+
+export const userSchema = new Schema<IUser>(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		admin: {
+			type: Boolean,
+			required: false,
+		},
+	},
+	{ timestamps: true }
+);
+
+export default model<IUser>('User', userSchema);

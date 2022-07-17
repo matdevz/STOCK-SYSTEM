f4c8d11f643400ad487ad66370cb6508004c8c76
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-17 15:47:06 -0400

    feat: função para validar os dados de login

--- system-backend/src/helpers/validateUser.ts
+++ system-backend/src/helpers/validateUser.ts
@@ -1,9 +1,19 @@
 import Joi from '@hapi/joi';
 import { IUser } from '../interfaces/IUser';
 
-export const userValidate = (data: IUser) => {
+export const registerValidate = (data: IUser) => {
 	const userSchema = Joi.object({
 		name: Joi.string().required().min(3).max(50),
+		email: Joi.string().email().required().min(3).max(50),
+		password: Joi.string().required().min(6),
+		admin: Joi.boolean(),
+	});
+
+	return userSchema.validate(data);
+};
+
+export const loginValidate = (data: IUser) => {
+	const userSchema = Joi.object({
 		email: Joi.string().email().required().min(3).max(50),
 		password: Joi.string().required().min(6),
 		admin: Joi.boolean(),

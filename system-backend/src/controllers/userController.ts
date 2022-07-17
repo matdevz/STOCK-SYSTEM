6b4fd7618b367e3acc3efdbf818359b41613be18
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-17 16:10:59 -0400

    feat: função de que faz o login do usuário e gera um token para o usuário

--- system-backend/src/controllers/userController.ts
+++ system-backend/src/controllers/userController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express';
 import { registerValidate, loginValidate } from '../helpers/validateUser';
-import { hash } from 'bcrypt';
+import { hashSync, compareSync } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
 import User from '../models/userModel';
 
 export default {
 	register: async (req: Request, res: Response): Promise<void> => {
 		const { error, value } = registerValidate(req.body);
 		const selectedUser = await User.findOne({ email: req.body.email });
-		const passwordHash = await hash(value.password, 10);
+		const passwordHash = hashSync(value.password, 10);
 
 		if (error) res.status(401).send('Houve um erro ao cadastrar usuário');
 		if (selectedUser) res.status(400).send('Esta conta já existe!');
@@ -16,17 +17,40 @@ export default {
 			name: value.name,
 			email: value.email,
 			password: passwordHash,
+			admin: false,
 		});
 
 		try {
 			const userSalved = await user.save();
-			console.log(userSalved);
-			res.json(user);
+			res.json(userSalved);
 		} catch (error) {
 			res.status(400).send(error);
 		}
 	},
 	login: async (req: Request, res: Response): Promise<void> => {
 		const { error, value } = loginValidate(req.body);
+		const selectedUser = await User.findOne({ email: req.body.email });
+
+		if (error) res.status(401).send('Houve um erro ao cadastrar usuário');
+		if (!selectedUser) res.status(400).send('Conta não foi cadastrada!');
+
+		const passwordUserMatch = compareSync(
+			value.password,
+			selectedUser!.password
+		);
+		if (!passwordUserMatch)
+			res.status(400).send('Email ou senha invalida!');
+
+		try {
+			const token = sign(
+				{ _id: selectedUser!._id, admin: selectedUser!.admin },
+				`${process.env.SECRET}`
+			);
+
+			res.header('authorization-token', token);
+			res.send('Login efetuado com sucesso!');
+		} catch (error) {
+			res.status(400).send(error);
+		}
 	},
 };

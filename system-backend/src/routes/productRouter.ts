8984053c20ccd3bcacec6ac72704d594b6d38262
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-19 22:19:23 -0400

    feat: implementando o authUser nas rotas dos produtos

--- system-backend/src/routes/productRouter.ts
+++ system-backend/src/routes/productRouter.ts
@@ -1,9 +1,10 @@
-import express from 'express';
-import productController from '../controllers/productController';
+import express from "express";
+import { authUser } from "../controllers/authController";
+import productController from "../controllers/productController";
 
 export const productRouter = express.Router();
 
-productRouter.post('/create', productController.create);
-productRouter.get('/read', productController.read);
-productRouter.put('/update/:id', productController.update);
-productRouter.delete('/delete/:id', productController.delete);
+productRouter.post("/create", authUser, productController.create);
+productRouter.get("/read", authUser, productController.read);
+productRouter.put("/update/:id", authUser, productController.update);
+productRouter.delete("/delete/:id", authUser, productController.delete);

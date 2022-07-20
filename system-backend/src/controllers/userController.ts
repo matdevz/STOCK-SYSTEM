fe4e5390178656d113dd74695330eaae37788017
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-19 22:16:18 -0400

    fix: corrigindo as respostas enviado pelos controllers

--- system-backend/src/controllers/userController.ts
+++ system-backend/src/controllers/userController.ts
@@ -1,56 +1,55 @@
-import { Request, Response } from 'express';
-import { registerValidate, loginValidate } from '../helpers/validateUser';
-import { hashSync, compareSync } from 'bcrypt';
-import { sign } from 'jsonwebtoken';
-import User from '../models/userModel';
+import { compareSync, hashSync } from "bcrypt";
+import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
+import { loginValidate, registerValidate } from "../helpers/validateUser";
+import User from "../models/userModel";
 
 export default {
-	register: async (req: Request, res: Response): Promise<void> => {
-		const { error, value } = registerValidate(req.body);
-		const selectedUser = await User.findOne({ email: req.body.email });
-		const passwordHash = hashSync(value.password, 10);
+  register: async (req: Request, res: Response): Promise<void> => {
+    const { error, value } = registerValidate(req.body);
+    const selectedUser = await User.findOne({ email: req.body.email });
+    const passwordHash = hashSync(value.password, 10);
 
-		if (error) res.status(401).send('Houve um erro ao cadastrar usuário');
-		if (selectedUser) res.status(400).send('Esta conta já existe!');
+    if (error) res.status(401).send("Houve um erro ao cadastrar usuário");
+    if (selectedUser) res.status(400).send("Esta conta já existe!");
 
-		const user = new User({
-			name: value.name,
-			email: value.email,
-			password: passwordHash,
-			admin: false,
-		});
+    const user = new User({
+      name: value.name,
+      email: value.email,
+      password: passwordHash,
+      admin: false,
+    });
 
-		try {
-			const userSalved = await user.save();
-			res.json(userSalved);
-		} catch (error) {
-			res.status(400).send(error);
-		}
-	},
-	login: async (req: Request, res: Response): Promise<void> => {
-		const { error, value } = loginValidate(req.body);
-		const selectedUser = await User.findOne({ email: req.body.email });
+    try {
+      const userSalved = await user.save();
+      res.json(userSalved);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  },
+  login: async (req: Request, res: Response): Promise<void> => {
+    const { error, value } = loginValidate(req.body);
+    const selectedUser = await User.findOne({ email: req.body.email });
 
-		if (error) res.status(401).send('Houve um erro ao cadastrar usuário');
-		if (!selectedUser) res.status(400).send('Conta não foi cadastrada!');
+    if (error) res.status(401).send({ error: "Erro ao cadastrar usuário" });
+    if (!selectedUser) res.status(400).send({ error: "Conta não existe!" });
 
-		const passwordUserMatch = compareSync(
-			value.password,
-			selectedUser!.password
-		);
-		if (!passwordUserMatch)
-			res.status(400).send('Email ou senha invalida!');
+    const passwordUserMatch = compareSync(
+      value.password,
+      selectedUser!.password
+    );
+    if (!passwordUserMatch)
+      res.status(400).send({ error: "Email ou senha invalida!" });
 
-		try {
-			const token = sign(
-				{ _id: selectedUser!._id, admin: selectedUser!.admin },
-				`${process.env.SECRET}`
-			);
+    try {
+      const token = sign(
+        { _id: selectedUser!._id, admin: selectedUser!.admin },
+        `${process.env.SECRET}`
+      );
 
-			res.header('authorization-token', token);
-			res.send('Login efetuado com sucesso!');
-		} catch (error) {
-			res.status(400).send(error);
-		}
-	},
+      res.status(200).json({ user: selectedUser, token });
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  },
 };

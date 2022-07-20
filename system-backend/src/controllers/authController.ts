21d430e4d16fe68b2382c591a068820b3a980fc7
Mateus Henrique <mateusdzn10@gmail.com>
2022-07-19 22:14:28 -0400

    feat: verificando se o usuário é um admin

--- system-backend/src/controllers/authController.ts
+++ system-backend/src/controllers/authController.ts
@@ -20,3 +20,20 @@ export const authUser = async (
     res.status(401).send({ error });
   }
 };
+
+export const authAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = req.user;
+  console.log(user);
+
+  if (!user.admin) return res.status(401).send({ error: "Acesso negado!" });
+
+  try {
+    next();
+  } catch (error) {
+    res.status(401).send({ error });
+  }
+};
